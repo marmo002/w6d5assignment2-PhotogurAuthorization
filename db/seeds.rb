a47bcbddfae82e9b0b2de89579d31742379b90f4
marmo002 <marmo_02@hotmail.com>
2017-10-10 16:05:43 -0400

    Add code to add pictures to my database, run the migrate method to actually add the pictures, run Pictures.all to see all  pictures on database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.create(
+  title: 'The old church on the coast of white sea',
+  artist: 'Serey Ershov',
+  url: 'http://bitmakerlasbs.s3.amazonaws.com/photogur/house.jpg'
+)
+
+Picture.create(
+  title: 'TSea Power',
+  artist: 'Stephen Scullion',
+  url: 'http://bitmakerlasbs.s3.amazonaws.com/photogur/wave.jpg'
+)
+
+Picture.create(
+  title: 'Into the poppies',
+  artist: 'John Wilhelm',
+  url: 'http://bitmakerlasbs.s3.amazonaws.com/photogur/girl.jpg'
+)
