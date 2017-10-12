22e8045a2cf85066bebddc618b16fd7a05956b63
marmo002 <marmo_02@hotmail.com>
2017-10-12 01:53:10 -0400

    add routes for edit = get 'pictures/:id/edit => 'pictures#edit' and update = patch 'pictures/:id' => 'pictures#update'. Create method edit on Pictures controller and save picture object into @picture variable. Create view file: 'edit.html.erb' and create form(form_for) to update current instance. Create update method, save instance into a variable and update instance with data passed from edit form. Redirect to picture 'show' page if saved, or render 'edit' page again

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
+  get 'pictures/:id/edit' => 'pictures#edit'
+  patch 'pictures/:id' => 'pictures#update'
 
   get 'pictures/:id' => 'pictures#show'
 
