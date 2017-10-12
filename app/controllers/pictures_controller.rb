b1293e820a7ea72a7246785ac5509aae52754ac4
marmo002 <marmo_02@hotmail.com>
2017-10-12 00:02:54 -0400

    on the create method inside the Pictures controller, add an instance variable(@picture) and store in ta variable a new instance of the Picture class. Pass on the Title, Artist, and URL from the new method(new.html.erb view) to the create method and store the data aas atributes of the new instance. Add if statement: if save redirect to index page and show all pictures, if not save call the new method again

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -14,5 +14,19 @@ class PicturesController < ApplicationController
 
   def create
     # render text: "Received POST request to '/pictures' with the data URL: #{params}"
+
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      #if the  pictures gets save, generate a fet request to "/pictures" (the index)
+      redirect_to '/pictures'
+    else
+      #otherwise render new.html.erb
+      render :new
+    end
   end
 end
